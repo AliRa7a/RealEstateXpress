f0c42f127a3d2029f8ff5729febd6ae38b5cec27
Ali Raza <alirazabinhussain@gmail.com>
2024-01-06 23:22:33 +0500

    Added Logout Route

--- resources/views/admin/layout/header.blade.php
+++ resources/views/admin/layout/header.blade.php
@@ -234,10 +234,13 @@
                             </a>
                         </li>
                         <li class="dropdown-item py-2">
-                            <a href="javascript:;" class="text-body ms-0">
-                                <i class="me-2 icon-md" data-feather="log-out"></i>
-                                <span>Log Out</span>
-                            </a>
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+                                <a href="javascript:;" class="text-body ms-0" onclick="event.preventDefault(); this.closest('form').submit();">
+                                    <i class="me-2 icon-md" data-feather="log-out"></i>
+                                    <span>Log Out</span>
+                                </a>
+                            </form>
                         </li>
                     </ul>
                 </div>
