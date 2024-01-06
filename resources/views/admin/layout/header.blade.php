d07c5860298c0b4678914d6b65479918ca208f85
Ali Raza <alirazabinhussain@gmail.com>
2024-01-06 23:34:23 +0500

    Added Custom Admin Logout Route

--- resources/views/admin/layout/header.blade.php
+++ resources/views/admin/layout/header.blade.php
@@ -234,13 +234,10 @@
                             </a>
                         </li>
                         <li class="dropdown-item py-2">
-                            <form method="POST" action="{{ route('logout') }}">
-                                @csrf
-                                <a href="javascript:;" class="text-body ms-0" onclick="event.preventDefault(); this.closest('form').submit();">
-                                    <i class="me-2 icon-md" data-feather="log-out"></i>
-                                    <span>Log Out</span>
-                                </a>
-                            </form>
+                            <a href="{{route('admin.logout')}}" class="text-body ms-0">
+                                <i class="me-2 icon-md" data-feather="log-out"></i>
+                                <span>Log Out</span>
+                            </a>
                         </li>
                     </ul>
                 </div>
