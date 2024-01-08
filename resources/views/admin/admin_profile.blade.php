fd404076cd17708aee46883ec049d9edc9f43eb7
Ali Raza <alirazabinhussain@gmail.com>
2024-01-08 22:07:26 +0500

    Load Admin Images and show no image if photo field is empty

--- resources/views/admin/admin_profile.blade.php
+++ resources/views/admin/admin_profile.blade.php
@@ -10,7 +10,7 @@
                 <div class="card-body">
                     <div class="d-flex align-items-center justify-content-between mb-2">
                         <div>
-                            <img class="wd-100 rounded-circle" src="https://via.placeholder.com/100x100" alt="profile">
+                            <img class="wd-100 rounded-circle" src="{{(!empty($profileData->photo)) ? url('upload/admin_images/'.$profileData->photo) : url('upload/no_image.jpg')}}" alt="profile">
                             <span class="h4 ms-3 text-dark">Amiah Burton</span>
                         </div>
 
