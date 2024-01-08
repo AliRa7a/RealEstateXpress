21c13802b9ecc94b3652d4c58468051673f25fe6
Ali Raza <alirazabinhussain@gmail.com>
2024-01-09 01:06:38 +0500

    Added update password route link

--- resources/views/admin/admin_chage_password.blade.php
+++ resources/views/admin/admin_chage_password.blade.php
@@ -53,7 +53,7 @@
 
                             <h6 class="card-title">Change Admin Password</h6>
 
-                            <form method="POST" action="{{route('admin.profile.store')}}" enctype="multipart/form-data" class="forms-sample">
+                            <form method="POST" action="{{route('admin.update.password')}}" class="forms-sample">
                                 @csrf
                                 <div class="mb-3">
                                     <label for="old_password" class="form-label">Old Password</label>
