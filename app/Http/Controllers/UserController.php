7fc43e25ee7eedf10a8bd5c4246f59f8872ec077
Ali Raza <alirazabinhussain@gmail.com>
2024-01-12 23:37:46 +0500

    Store User Profile data to update in database

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -19,4 +19,26 @@ class UserController extends Controller
         $userData = User::find($id);
         return view('frontend.dashboard.user_profile', compact('userData'));
     }
+
+    public function userProfileStore(Request $request)
+    {
+        $id = Auth::user()->id;
+        $data = User::find($id);
+        $data->username = $request->username;
+        $data->name = $request->name;
+        $data->phone = $request->phone;
+        $data->email = $request->email;
+        $data->address = $request->address;
+
+        if ($request->file('photo')) {
+            $file = $request->file('photo');
+            @unlink(public_path('upload/user_images/' . $data->photo));
+            $fileName = date('YmdHi') . $file->getClientOriginalName();
+            $file->move(public_path('upload/user_images'), $fileName);
+            $data['photo'] = $fileName;
+        }
+
+        $data->save();
+        return redirect()->back();
+    }
 }
