3aa2cc36f700eed8e7f4e97cc42341003cb14814
Ali Raza <alirazabinhussain@gmail.com>
2024-01-12 22:36:34 +0500

    User Profile Controller to show user data

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
     public function index()
     {
         return view('frontend.index');
+    }
+
+    public function userProfile()
+    {
+        $id = Auth::user()->id;
+        $userData = User::find($id);
+        return view('frontend.dashboard.user_profile', compact($userData));
     }
 }
