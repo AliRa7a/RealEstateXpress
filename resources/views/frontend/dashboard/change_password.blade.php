934009760cf526331b5abcf63474da8fd82a4548
Ali Raza <alirazabinhussain@gmail.com>
2024-01-13 17:37:53 +0500

    Update Password Route added

--- resources/views/frontend/dashboard/change_password.blade.php
+++ resources/views/frontend/dashboard/change_password.blade.php
@@ -31,29 +31,36 @@
                         <div class="inner-box">
 
                             <div class="lower-content">
-                                <form method="POST" action="" class="forms-sample">
+                                <form method="POST" action="{{ route('user.update.password') }}">
                                     @csrf
-                                    <div class="mb-3">
-                                        <label for="old_password" class="form-label">Old Password</label>
-                                        <input type="password" class="form-control" @error('old_password') is-invalid @enderror name="old_password" id="old_password" autocomplete="off">
-                                        @error('old_password')
-                                        <span class=text-danger>{{$message}}</span>
+
+                                    <div class="form-group">
+                                        <label for="current_password">Current Password:</label>
+                                        <input type="password" name="current_password" id="current_password" class="form-control">
+                                        @error('current_password')
+                                        <span class="text-danger">{{ $message }}</span>
                                         @enderror
-                                    </div>
-                                    <div class="mb-3">
-                                        <label for="new_password" class="form-label">New Password</label>
-                                        <input type="password" class="form-control" @error('new_password') is-invalid @enderror name="new_password" id="new_password" autocomplete="off">
-                                        @error('new_password')
-                                        <span class=text-danger>{{$message}}</span>
-                                        @enderror
-                                    </div>
-                                    <div class="mb-3">
-                                        <label for="confirm_password" class="form-label">Confirm Password</label>
-                                        <input type="password" class="form-control" name="confirm_password" id="confirm_password" autocomplete="off">
                                     </div>
 
-                                    <button type="submit" class="btn btn-primary me-2">Change Password</button>
+                                    <div class="form-group">
+                                        <label for="new_password">New Password:</label>
+                                        <input type="password" name="new_password" id="new_password" class="form-control">
+                                        @error('new_password')
+                                        <span class="text-danger">{{ $message }}</span>
+                                        @enderror
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="confirm_password">Confirm Password:</label>
+                                        <input type="password" name="confirm_password" id="confirm_password" class="form-control">
+                                        @error('confirm_password')
+                                        <span class="text-danger">{{ $message }}</span>
+                                        @enderror
+                                    </div>
+
+                                    <button type="submit" class="btn btn-primary">Update Password</button>
                                 </form>
+
                             </div>
                         </div>
                     </div>
