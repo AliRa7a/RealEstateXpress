dd99aecbe4a9decb2883d86b5c62f4751e37afbc
Ali Raza <alirazabinhussain@gmail.com>
2024-01-14 22:59:12 +0500

    All Types Property Controller added to get the latest data and return the view

--- app/Http/Controllers/Backend/propertyTypeController.php
+++ app/Http/Controllers/Backend/propertyTypeController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\PropertyType;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class propertyTypeController extends Controller
 {
-    //
+    public function AllType()
+    {
+        $propertyTypes = PropertyType::latest()->get();
+        return view('admin.property_type.all_type', compact('propertyTypes'));
+    }
 }
