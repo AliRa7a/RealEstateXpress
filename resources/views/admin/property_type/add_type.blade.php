28f6039c607a6ddec4cb8726d5c86af6a1d5d39f
Ali Raza <alirazabinhussain@gmail.com>
2024-01-15 23:11:17 +0500

    Store Property Type link and fields added

--- resources/views/admin/property_type/add_type.blade.php
+++ resources/views/admin/property_type/add_type.blade.php
@@ -13,20 +13,18 @@
 
                             <h6 class="card-title">Add Property Type</h6>
 
-                            <form method="POST" action="{{route('admin.update.password')}}" class="forms-sample">
+                            <form method="POST" action="{{route('store.type')}}" class="forms-sample">
                                 @csrf
                                 <div class="mb-3">
                                     <label for="type_name" class="form-label">Type Name</label>
-                                    <input type="text" class="form-control" name="type_name" id="type_name" autocomplete="off">
+                                    <input type="text" class="form-control" name="type_name" id="type_name" required>
                                 </div>
                                 <div class="mb-3">
                                     <label for="type_icon" class="form-label">Type Icon</label>
-                                    <input type="file" class="form-control" name="type_icon" id="type_icon" autocomplete="off">
+                                    <input type="text" class="form-control" name="type_icon" id="type_icon" required>
                                 </div>
-
                                 <button type="submit" class="btn btn-primary me-2">Add</button>
                             </form>
-
                         </div>
                     </div>
                 </div>
