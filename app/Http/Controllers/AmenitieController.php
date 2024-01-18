48de2ca14d899f8d028418d3ccb910eecb1d434b
Ali Raza <alirazabinhussain@gmail.com>
2024-01-19 00:07:23 +0500

    Funciton to Store Amenities in DB

--- app/Http/Controllers/AmenitieController.php
+++ app/Http/Controllers/AmenitieController.php
@@ -10,12 +10,24 @@ class AmenitieController extends Controller
     public function allAmenities()
     {
         $amenityTypes = Amenities::latest()->get();
+        foreach($amenityTypes as $amenityType){
+            $amenityType->amenities_name;
+        }
         return view('admin.amenities.all_type', compact('amenityTypes'));
     }
     public function addAmenities()
     {
         return view('admin.amenities.add_type');
     }
-
-
+    public function storeAmenities(Request $request)
+    {
+        $request->validate([
+            'amenities_name' => 'required|unique:amenities|max:200',
+        ]);
+        Amenities::insert([
+            'amenities_name' => $request->amenities_name,
+        ]);
+        toastr()->success('New Amenity is added successfully');
+        return redirect()->route('all.type');
+    }
 }
