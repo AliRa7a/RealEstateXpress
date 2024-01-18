e57eff611f177060e7c515b9fff7c30e7c236240
Ali Raza <alirazabinhussain@gmail.com>
2024-01-18 23:49:24 +0500

    Add Amenities link added

--- resources/views/admin/amenities/all_type.blade.php
+++ resources/views/admin/amenities/all_type.blade.php
@@ -3,14 +3,14 @@
 <div class="page-content">
 
     <nav class="page-breadcrumb">
-        <a href="{{route('add.type')}}" type="button" class="btn btn-inverse-info">Add Amenity</a>
+        <a href="{{route('add.amenities')}}" type="button" class="btn btn-inverse-info">Add Amenity</a>
     </nav>
 
     <div class="row">
         <div class="col-md-12 grid-margin stretch-card">
             <div class="card">
                 <div class="card-body">
-                    <h6 class="card-title">All Property Types</h6>
+                    <h6 class="card-title">All Amenities</h6>
                     <div class="table-responsive">
                         <table id="dataTableExample" class="table">
                             <thead>
