3efe57fb5899b29eeedd707fd0936db7a16b9691
Ali Raza <alirazabinhussain@gmail.com>
2024-01-20 02:57:30 +0500

    All Properties Functions to get latest data and return the view

--- app/Http/Controllers/Backend/PropertyController.php
+++ app/Http/Controllers/Backend/PropertyController.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Facility;
+use App\Models\MultiImage;
+use App\Models\Property;
 use Illuminate\Http\Request;
+
 
 class PropertyController extends Controller
 {
-    //
+    public function allProperties()
+    {
+        $properties = Property::latest()->get();
+        return view('admin.properties.all_properties', compact('properties'));
+    }
 }
