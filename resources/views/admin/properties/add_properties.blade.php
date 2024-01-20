f05d87ca1973f28c4882a277b9e55d83e1a633ae
Ali Raza <alirazabinhussain@gmail.com>
2024-01-21 00:55:43 +0500

    Add Properties Fileds added

--- resources/views/admin/properties/add_properties.blade.php
+++ resources/views/admin/properties/add_properties.blade.php
@@ -10,17 +10,90 @@
                 <div class="col-md-12 grid-margin">
                     <div class="card">
                         <div class="card-body">
-
                             <h6 class="card-title">Add Properties</h6>
-
-                            <form method="POST" action="" class="forms-sample">
-                                @csrf
-                                <div class="mb-3">
-                                    <label for="" class="form-label">Amenity Name</label>
-                                    <input type="text" class="form-control" name="" id="" required>
-                                </div>
-                                <button type="submit" class="btn btn-primary me-2">Add</button>
+                            <form>
+                                <div class="row">
+                                    <div class="col-sm-6">
+                                        <div class="mb-3">
+                                            <label class="form-label">Property Name</label>
+                                            <input type="text" name="property_name" class="form-control">
+                                        </div>
+                                    </div><!-- Col -->
+                                    <div class="col-sm-6">
+                                        <div class="mb-3">
+                                            <label for="property_status" class="form-label">Property Status</label>
+                                            <select class="form-select" name="property_status" id="property_status">
+                                                <option selected disabled>Select Status</option>
+                                                <option value="rent">Rent</option>
+                                                <option value="buy">Buy</option>
+                                            </select>
+                                        </div>
+                                    </div><!-- Col -->
+                                </div><!-- Row -->
+                                <div class="row">
+                                    <div class="col-sm-6">
+                                        <div class="mb-3">
+                                            <label class="form-label">Min Price</label>
+                                            <input type="text" name="min_price" class="form-control">
+                                        </div>
+                                    </div><!-- Col -->
+                                    <div class="col-sm-6">
+                                        <div class="mb-3">
+                                            <label class="form-label">Max Price</label>
+                                            <input type="text" name="max_price" class="form-control">
+                                        </div>
+                                    </div><!-- Col -->
+                                </div><!-- Row -->
+                                <div class="row">
+                                    <div class="col-sm-6">
+                                        <div class="mb-3">
+                                            <label class="form-label">Main Image</label>
+                                            <input type="file" name="min_price" class="form-control">
+                                        </div>
+                                    </div><!-- Col -->
+                                    <div class="col-sm-6">
+                                        <div class="mb-3">
+                                            <label class="form-label">Additional Images</label>
+                                            <input type="file" name="max_price" class="form-control">
+                                        </div>
+                                    </div><!-- Col -->
+                                </div><!-- Row -->
+                                <div class="row">
+                                    <div class="col-sm-4">
+                                        <div class="mb-3">
+                                            <label class="form-label">City</label>
+                                            <input type="text" class="form-control" placeholder="Enter city">
+                                        </div>
+                                    </div><!-- Col -->
+                                    <div class="col-sm-4">
+                                        <div class="mb-3">
+                                            <label class="form-label">State</label>
+                                            <input type="text" class="form-control" placeholder="Enter state">
+                                        </div>
+                                    </div><!-- Col -->
+                                    <div class="col-sm-4">
+                                        <div class="mb-3">
+                                            <label class="form-label">Zip</label>
+                                            <input type="text" class="form-control" placeholder="Enter zip code">
+                                        </div>
+                                    </div><!-- Col -->
+                                </div><!-- Row -->
+                                <div class="row">
+                                    <div class="col-sm-6">
+                                        <div class="mb-3">
+                                            <label class="form-label">Email address</label>
+                                            <input type="email" class="form-control" placeholder="Enter email">
+                                        </div>
+                                    </div><!-- Col -->
+                                    <div class="col-sm-6">
+                                        <div class="mb-3">
+                                            <label class="form-label">Password</label>
+                                            <input type="password" class="form-control" autocomplete="off" placeholder="Password">
+                                        </div>
+                                    </div><!-- Col -->
+                                </div><!-- Row -->
                             </form>
+                            <button type="button" class="btn btn-primary submit">Submit form</button>
                         </div>
                     </div>
                 </div>
