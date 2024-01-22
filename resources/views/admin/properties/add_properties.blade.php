633b53ae6e538496762a3abb337fa5c4d59a8186
Ali Raza <alirazabinhussain@gmail.com>
2024-01-22 05:15:09 +0500

    Facilities Fields are added

--- resources/views/admin/properties/add_properties.blade.php
+++ resources/views/admin/properties/add_properties.blade.php
@@ -218,6 +218,37 @@
                                         </label>
                                     </div>
                                 </div>
+
+                                <div class="row add_item">
+                                    <div class="col-md-4">
+                                        <div class="mb-3">
+                                            <label for="facility_name" class="form-label">Facilities </label>
+                                            <select name="facility_name[]" id="facility_name" class="form-control">
+                                                <option value="">Select Facility</option>
+                                                <option value="Hospital">Hospital</option>
+                                                <option value="SuperMarket">Super Market</option>
+                                                <option value="School">School</option>
+                                                <option value="Entertainment">Entertainment</option>
+                                                <option value="Pharmacy">Pharmacy</option>
+                                                <option value="Airport">Airport</option>
+                                                <option value="Railways">Railways</option>
+                                                <option value="Bus Stop">Bus Stop</option>
+                                                <option value="Beach">Beach</option>
+                                                <option value="Mall">Mall</option>
+                                                <option value="Bank">Bank</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="mb-3">
+                                            <label for="distance" class="form-label"> Distance </label>
+                                            <input type="text" name="distance[]" id="distance" class="form-control" placeholder="Distance (Km)">
+                                        </div>
+                                    </div>
+                                    <div class="form-group col-md-4" style="padding-top: 30px;">
+                                        <a class="btn btn-success addeventmore"><i class="fa fa-plus-circle"></i> Add More..</a>
+                                    </div>
+                                </div> <!---end row-->
                             </form>
 
 
