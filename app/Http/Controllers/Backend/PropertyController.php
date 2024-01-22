06b951a0b0e1c98de1e11b7b61033496bc6927db
Ali Raza <alirazabinhussain@gmail.com>
2024-01-23 01:47:11 +0500

    Store Properties Function to save properties data

--- app/Http/Controllers/Backend/PropertyController.php
+++ app/Http/Controllers/Backend/PropertyController.php
@@ -9,8 +9,10 @@ use App\Models\MultiImage;
 use App\Models\Property;
 use App\Models\PropertyType;
 use App\Models\User;
+use Haruncpi\LaravelIdGenerator\IdGenerator;
 use Illuminate\Http\Request;
 use Intervention\Image\Facades\Image;
+
 
 class PropertyController extends Controller
 {
@@ -30,9 +32,41 @@ class PropertyController extends Controller
 
     public function storeProperties(Request $request)
     {
+        $amen = $request->amenities_id;
+        $amenities = implode(",", $amen);
+
+        $pcode = IdGenerator::generate(['table' => 'properties', 'field' => 'property_code', 'length' => 5, 'prefix' => 'PC']);
         $image = $request->file('property_thumbnail');
         $name_gen = hexdec(uniqid()) . '.' . $image->getClientOriginalExtension();
         Image::make($image)->resize(370, 250)->save('upload/property/thumbnail/' . $name_gen);
         $save_url = 'upload/property/thumbnail/' . $name_gen;
+
+        $property_id = Property::insertGetId([
+            'propertytype_id' => $request->propertytype_id,
+            'amenities_id' => $request->$amenities,
+            'property_name' => $request->property_name,
+            'property_slug' => strtolower(str_replace(' ', '-', $request->property_name)),
+            'propertytype_id' => $pcode,
+
+            'property_status' => $request->property_status,
+            'min_price' => $request->min_price,
+            'max_price' => $request->max_price,
+            'short_description' => $request->short_description,
+            'long_description' => $request->long_description,
+
+            'propertytype_id' => $request->propertytype_id,
+            'propertytype_id' => $request->propertytype_id,
+            'propertytype_id' => $request->propertytype_id,
+            'propertytype_id' => $request->propertytype_id,
+            'propertytype_id' => $request->propertytype_id,
+
+            'propertytype_id' => $request->propertytype_id,
+            'propertytype_id' => $request->propertytype_id,
+            'propertytype_id' => $request->propertytype_id,
+            'propertytype_id' => $request->propertytype_id,
+            'propertytype_id' => $request->propertytype_id,
+            'propertytype_id' => $request->propertytype_id,
+
+        ]);
     }
 }
